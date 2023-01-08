e04e4754a5ab6be874adc0bc14b5984cc8bb4e29
Andriță Lucian-Gabriel <gabylucian2000@gmail.com>
2023-01-08 18:42:48 +0200

    Adaugat Identity, dar inca nu merge

---
+++ Areas/Identity/Model/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Uichipedia.Areas.Identity.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string? Category { get; set; }
+        public string? Content { get; set; }   //The question mark after string indicates that the property is nullable
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using Uichipedia.Data;
 using Uichipedia.Model;
 namespace Uichipedia
@@ -9,11 +9,53 @@ namespace Uichipedia
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddDbContext<UichipediaContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("UichipediaContext") ?? throw new InvalidOperationException("Connection string 'UichipediaContext' not found.")));
+            var connectionString = builder.Configuration.GetConnectionString("UichipediaContext");
 
+            builder.Services.AddDbContext<UichipediaContext>(options =>
+                options.UseSqlServer(connectionString ?? throw new InvalidOperationException("Connection string 'UichipediaContext' not found.")));
+
+            // =====================================
+            builder.Services.AddDbContext<UserContext>(options =>
+            options.UseSqlServer(connectionString));
+            // builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<UserContext>();
+
+            // =====================================
             // Add services to the container.
             builder.Services.AddRazorPages();
+
+            builder.Services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings.
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+
+                // Lockout settings.
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                options.Lockout.AllowedForNewUsers = true;
+
+                // User settings.
+                options.User.AllowedUserNameCharacters =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = false;
+            });
+
+            builder.Services.ConfigureApplicationCookie(options =>
+            {
+                // Cookie settings
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+
+                options.LoginPath = "/Identity/Account/Login";
+                options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+                options.SlidingExpiration = true;
+            });
 
             var app = builder.Build();
 
@@ -36,6 +78,7 @@ namespace Uichipedia
             app.UseStaticFiles();
 
             app.UseRouting();
+            app.UseAuthentication(); ;
 
             app.UseAuthorization();
 
